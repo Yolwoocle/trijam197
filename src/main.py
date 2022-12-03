1a33ec4360c51a40e800bbfe7942ebc5d7615393
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2022-12-03 17:00:42 +0100

    Vector3

--- src/main.py
+++ src/main.py
@@ -5,16 +5,16 @@ def load_image(path):
 
 class Object:
     def __init__(self, x=0, y=0) -> None:
-        self.pos =  pygame.math.Vector2(x, y)
         self.size = pygame.math.Vector2(50, 50)
-        self.vel =  pygame.math.Vector2(0, 0)
-        self.acc =  pygame.math.Vector2(0, 0)
+        self.pos =  pygame.math.Vector3(x, y, 0)
+        self.vel =  pygame.math.Vector3(0, 0, 0)
+        self.acc =  pygame.math.Vector3(0, 0, 0)
         self.forces = []
         self.friction = 0.9
         self.mass = 10
 
     def update(self):
-        self.acc = pygame.math.Vector2(0, 0)
+        self.acc = pygame.math.Vector3(0, 0, 0)
         for f in self.forces:
             self.acc = self.acc + f
         if(self.vel.length()>0):
@@ -22,13 +22,16 @@ class Object:
             self.acc -= self.friction * self.vel.normalize()*(self.vel.length()**2)
         self.vel += self.acc / self.mass
         if(self.vel.length()<=0.1):
-            self.vel = pygame.math.Vector2(0, 0)
+            self.vel = pygame.math.Vector3(0, 0, 0)
         self.pos += self.vel
 
 class Image:
     slimes = [
         load_image('img/slime1.png'),
         load_image('img/slime2.png'),
+    ]
+    balls = [
+        load_image('img/ball1.png'),
     ]
 
 class Animated(Object):
@@ -38,11 +41,12 @@ class Animated(Object):
         self.sprites = []
         self.current_sprite = 0
         self.elapsed = 0
+        self.sprite_offset = pygame.math.Vector2(0,0)
         
     def draw(self):
         if len(self.sprites)>self.current_sprite:
             self.sprites[self.current_sprite] = pygame.transform.scale(self.sprites[self.current_sprite], (self.size.x, self.size.y))
-            screen.blit(self.sprites[self.current_sprite], self.pos-self.size/2)
+            screen.blit(self.sprites[self.current_sprite], self.pos + self.sprite_offset - self.size/2)
             if self.elapsed > 10:
                 self.current_sprite=(self.current_sprite+1)%len(self.sprites)
                 self.elapsed = 0
@@ -56,13 +60,13 @@ class Player(Animated):
 
         self.friction = 0.97
         self.speed = 10
-        self.forces = [pygame.math.Vector2(0, 0)]
+        self.forces = [pygame.math.Vector3(0,0,0)]
 
     def update(self):
         super().update()
 
         keys = pygame.key.get_pressed()
-        self.forces[0] = pygame.math.Vector2(0, 0)
+        self.forces[0] = pygame.math.Vector3()
         if keys[pygame.K_LEFT]: # We can check if a key is pressed like this
             self.forces[0].x += -1
             self.forces[0].y += 0
@@ -89,9 +93,19 @@ class Player(Animated):
 class Ball(Animated):
     def __init__(self, initSize=10):
         self.size = initSize
+        self.sprites = Image.balls
+        self.sprite_offset = pygame.math.Vector2(0,0)
+        self.forces = [pygame.math.Vector3()]
+        
+        self.mass = 10
     
-    def split(self):
-        self.size /= 2
+    def update(self):
+        super().update()
+        self.sprite_offset = pygame.math.Vector2(0, -self.altitude)
+        
+
+    def draw(self):
+        super.draw()
 
 
 pygame.init()
