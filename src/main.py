b5ea2c107cd22c10f3597595b73216465e777e4c
rems64 <rems64des@gmail.com>
2022-12-03 16:27:49 +0100

    grzg

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ class Object:
         self.size = pygame.math.Vector2(50, 50)
         self.vel =  pygame.math.Vector2(0, 0)
         self.acc =  pygame.math.Vector2(0, 0)
+        self.input_force = pygame.math.Vector2(0, 0)
 
     def update(self):
         self.pos += self.vel * self.friction
