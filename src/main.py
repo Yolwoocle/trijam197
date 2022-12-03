c8fcf6eb74b8312d65d6575a1595c837b7fe2a1e
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2022-12-03 16:17:29 +0100

    Prout

--- src/main.py
+++ src/main.py
@@ -8,16 +8,8 @@ class Object:
         self.pos = pygame.math.Vector2(x, y)
         self.vel = pygame.math.Vector2(0, 0)
 
-
 class Image:
     test = load_image('img/test.png')
-
-
-    def update(self):
-        ...
-    
-    def draw(self):
-        super().draw()
 
 
 class Animated(Object):
@@ -32,9 +24,20 @@ class Animated(Object):
 class Player(Animated):
     def __init__(self, x, y) -> None:
         super().__init__(x, y)
-        self.image = Image.test
+        self.sprites = [Image.test]
 
-class Ball(Object):
+        self.image = Image.test
+        self.friction = 0.97
+
+    def update(self):
+        self.pos += self.vel * self.friction
+
+    def draw(self):
+        ...
+
+        
+
+class Ball(Animated):
     def __init__(self, initSize=10):
         self.size = initSize
     
@@ -45,26 +48,23 @@ class Ball(Object):
 pygame.init()
 
 # Define some colors
-black = ( 0, 0, 0)
-white = ( 255, 255, 255)
-green = ( 0, 255, 0)
-red   = ( 255, 0, 0)
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+red   = (255, 0, 0)
 
-# Open a new window
 size = (700, 500)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My First Game")
+pygame.display.set_caption("Trijam 197")
 
-# The loop will carry on until the user exits the game (e.g. clicks the close button).
 carryOn = True
 
-# The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
 
-player = Player(size[0]/2, size[1]/2)
-
 # -------- Main Program Loop -----------
-objects = []
+objects = [
+    Player(size[0]/2, size[1]/2)
+]
 
 while carryOn:
     for event in pygame.event.get():
@@ -75,16 +75,11 @@ while carryOn:
     for object in objects:
         object.update()
 
-
     # --- Drawing code should go here
     for object in objects:
         screen.fill(WHITE)
         object.draw()
     
-    # pygame.draw.rect(screen, RED, [55, 200, 100, 70],0)
-    # pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    # pygame.draw.ellipse(screen, BLACK, [20,20,250,100], 2)
-
     pygame.display.flip()
 
     clock.tick(60)
